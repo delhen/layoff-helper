9ba39314229c20af74d2b0e07eaf029f3532066b
delhen <hendardidel@gmail.com>
2022-06-27 22:50:12 +0700

    Added Landing Page

---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './LandingPage.module.css';
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div className={styles.landingPage}>
+        <div>LandingPage</div>
+      </div>
+    )
+  }
+}
