4249c876fbd64d9f5bf31bfe3891c9d9855de99b
delhen <hendardidel@gmail.com>
2022-07-01 22:56:36 +0700

    Added routing and minor changes

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -3,21 +3,43 @@ import BigHeaderText from '../../components/BigHeaderText/BigHeaderText';
 import Button from '../../components/Button/Button';
 import MediumHeaderText from '../../components/MediumHeaderText/MediumHeaderText';
 import styles from './LandingPage.module.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import EmployeeFormPage from '../EmployeeFormPage/EmployeeFormPage';
+
+class LandingPageContent extends Component {
+  render() {
+    return (
+      <>
+        <Link to="/r/employee">
+          <Button text="Affected Employee" color="blue" />
+        </Link>
+        <Link to="/r/talent">
+          <Button text="Talent Acquistion" color="blue" />
+        </Link>
+      </>
+    )
+  }
+}
+
 
 export default class LandingPage extends Component {
   render() {
     return (
       <div className={styles.landingPage}>
-        <div className={styles.paddingPageLayout}>
+        <div className={styles.pageLayout}>
           <BigHeaderText text="We will help you!" />
+          <br></br>
           <MediumHeaderText text="We know how the feel when getting laid off ☹️" />
-          <div className='mt-8'>
+          <div className='mt-4'>
             <h3 className='text-xl mb-2 font-semibold'>Register as:</h3>
-            <Button text="Affected Employee" color="blue" />
-            <Button text="Talent Acquistion" color="blue" />
+            <Routes>
+              <Route path='/' element={<LandingPageContent />} />
+              <Route path='/r/employee' element={<EmployeeFormPage />} />
+            </Routes>
           </div>
         </div>
 
+        {/* Clouds */}
         <div className={styles.x1}>
           <div className={styles.cloud}></div>
         </div>
