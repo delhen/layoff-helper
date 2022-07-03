fe8143b270da9d109e2c2073c97707e095c80371
delhen <hendardidel@gmail.com>
2022-07-03 18:00:59 +0700

    Adjustment landing page routing and component generation

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -4,9 +4,11 @@ import Button from '../../components/Button/Button';
 import MediumHeaderText from '../../components/MediumHeaderText/MediumHeaderText';
 import styles from './LandingPage.module.css';
 import { Routes, Route, Link } from 'react-router-dom';
-import EmployeeFormPage from '../EmployeeFormPage/EmployeeFormPage';
+import EmployeeFormPage from './EmployeeFormPage/EmployeeFormPage';
+import TalentFormPage from './TalentFormPage/TalentFormPage';
+import RegisterSuccessPage from './RegisterSuccessPage/RegisterSuccessPage';
 
-class LandingPageContent extends Component {
+class RegisterComponent extends Component {
   render() {
     return (
       <>
@@ -21,25 +23,54 @@ class LandingPageContent extends Component {
   }
 }
 
-
-export default class LandingPage extends Component {
+class LandingPageRoutes extends Component {
   render() {
     return (
-      <div className={styles.landingPage}>
-        <div className={styles.pageLayout}>
-          <BigHeaderText text="We will help you!" />
-          <br></br>
-          <MediumHeaderText text="We know how the feel when getting laid off ☹️" />
-          <div className='mt-4'>
-            <h3 className='text-xl mb-2 font-semibold'>Register as:</h3>
-            <Routes>
-              <Route path='/' element={<LandingPageContent />} />
-              <Route path='/r/employee' element={<EmployeeFormPage />} />
-            </Routes>
-          </div>
-        </div>
+      <>
+        <Routes>
+          <Route path='/' exact element={<LandingPageContent />} />
+          <Route path='/*' element={<LandingPageContent />} />
+          <Route path='/r/success' exact element={<RegisterSuccessPage />} />
+        </Routes>
+      </>
+    )
+  }
+}
 
-        {/* Clouds */}
+class RegisterRoutes extends Component {
+  render() {
+    return (
+      <>
+        <Routes>
+          <Route path='/' element={<RegisterComponent />} />
+          <Route path='/r/employee' exact element={<EmployeeFormPage />} />
+          <Route path='/r/talent' exact element={<TalentFormPage />} />
+        </Routes>
+      </>
+    )
+  }
+}
+
+class LandingPageContent extends Component {
+  render() {
+    return (
+      <>
+        <BigHeaderText text="We will help you!" />
+        <br></br>
+        <MediumHeaderText text="We know how the feel when getting laid off ☹️" />
+        <div className='mt-4'>
+          <h3 className='text-xl mb-2 font-semibold'>Register as:</h3>
+          <RegisterRoutes />
+        </div>
+      </>
+    )
+  }
+}
+
+class Clouds extends Component {
+  render() {
+    return (
+      <>
         <div className={styles.x1}>
           <div className={styles.cloud}></div>
         </div>
@@ -52,6 +83,20 @@ export default class LandingPage extends Component {
         <div className={styles.x4}>
           <div className={styles.cloud}></div>
         </div>
+      </>
+    )
+  }
+}
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div className={styles.landingPage}>
+        <div className={styles.pageLayout}>
+          <LandingPageRoutes />
+        </div>
+
+        <Clouds />
         {/* For Bottom Image */ }
         <div className={styles.image}></div>
       </div>
